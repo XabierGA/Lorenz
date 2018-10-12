fe6a9348206aa1294c76f8aedcf2b340b7d4eb19
XabierGA <39628339+XabierGA@users.noreply.github.com>
2018-10-12 21:26:04 +0200

    Update lorenzattractor.py

--- lorenzattractor.py
+++ lorenzattractor.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from mpl_toolkits.mplot3d import Axes3D
 
-rho = 28.0
-sigma = 10.0
+rho = 27.0
+sigma = 10.5
 beta = 8.0 / 3.0
 
 def f(state, t):
-  x, y, z = state  # unpack the state vector
-  return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  # derivatives
+  x, y, z = state  
+  return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  
 
 state0 = [1.0, 1.0, 1.0]
-t = np.arange(0.0, 40.0, 0.01)
+t = np.linspace(0 , 40 , 100000)
 
 states = odeint(f, state0, t)
 
